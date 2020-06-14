b2ddda2e2c17b74007f28884559594f566e5cd50
varsha kumari <varsha.kumari_cs17@gla.ac.in>
2020-06-14 20:19:51 +0530

    Add files via upload

---
+++ src/main/java/db/migration/V1__.sql
@@ -0,0 +1,7 @@
+create table user (
+id integer UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name varchar(50),email varchar(50),
+   created_date date,updated_date date, created_by varchar(20),updated_by varchar(20),password varchar(255),
+ username varchar(255)
+    
+);
